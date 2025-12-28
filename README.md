5d46ba1596a1e4f3dcdc3e53d9442e0693a7df4b
Karim ElGhandour <kghandour@live.com>
2025-12-28 13:21:01 +0100

    Hide generated password by default

---
+++ README.md
@@ -0,0 +1,12 @@
+# KG Password Generator written in Rust
+Follows the original dart and flutter implementation ([Here](https://github.com/kghandour/kgpassgen)). 
+
+This rust implementation aims to provide a CLI password generator and a cargo implementation to be used in other packages when needed.
+
+For the CLI implementation, by default, the password input is hidden from the terminal and the generated input gets automatically copied to clipboard without getting displayed to the terminal. This is done as a security feature to prevent possible logging to history.
+
+## FAQ
+1. If you are on Wayland and copying to clipboard does not work
+- This app uses `arboard` to manage the clipboard ([Here](https://github.com/1Password/arboard)). If there is a problem, it is recommended to use Xwayland instead. If the problem persists or you are facing it in other operating systems, please create an issue. 
+
+- An alternative solution that is currently WIP
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::io;
+use arboard::Clipboard;
 
 fn main() {
+    let mut clipboard = Clipboard::new().unwrap();
+
     println!("===== KG Password Generator =====");
     println!("Enter the service url you want to generate a password for");
     println!("Service URL:");
@@ -12,6 +15,23 @@ fn main() {
 
     let master_password = rpassword::prompt_password("Enter master password:\n").unwrap();
 
-    println!("Generating an MD5 hash for {}:{}", url.trim(), master_password.trim());
+    let generated_password = generate_password(&url, &master_password);
+    clipboard.set_text(&generated_password).unwrap();
+    println!("Generated password copied to clipboard!");
 
+    println!("Do you want to see the generated password? (y/n) [Default: n]:");
+    let mut show_password = String::new();
+    io::stdin().read_line(&mut show_password)
+        .expect("Failed to read input");
+    if show_password.trim().to_lowercase() == "y" || show_password.trim().to_lowercase() == "yes" {
+        println!("Generated password: {}", generated_password.trim());
+    } else {
+        println!("Password not displayed.")
+    }
+
+}
+
+fn generate_password(url: &str, master_password: &str) -> String {
+    // Placeholder for password generation logic
+    return format!("{}:{}", url.trim(), master_password.trim())
 }
